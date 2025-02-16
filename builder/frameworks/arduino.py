f3b5e954a3d5d6c0fed1e59b8dc2cce4dc4ff658
Crazy-Charles <119892416+Crazy-Charles@users.noreply.github.com>
2025-02-16 09:30:02 +0000

    Initial Upload

---
+++ builder/frameworks/arduino.py
@@ -0,0 +1,14 @@
+import sys
+from os.path import join, isfile
+
+from SCons.Script import DefaultEnvironment, SConscript
+
+
+env = DefaultEnvironment()
+build_script = join(env.PioPlatform().get_package_dir("framework-arduino-n32g45x"), "tools", "platformio", "platformio-build-arduino.py")
+
+if not isfile(build_script):
+    sys.stderr.write(f"Error: Missing PlatformIO build script {build_script}!\n")
+    env.Exit(1)
+
+SConscript(build_script)
